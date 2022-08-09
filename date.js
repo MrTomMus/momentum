cd2c770d7a4bfe89543fb54ad6edf9ebdb9a5b4b
MrTomMus <75030876+MrTomMus@users.noreply.github.com>
2022-08-09 20:12:28 +0300

    fix: fixed date file

--- date.js
+++ date.js
@@ -1,10 +1,31 @@
 let timeDiv = document.querySelector('.time');
 let dateDiv = document.querySelector('.date');
 
+let options = {
+    month: 'long',
+    day: 'numeric', 
+    hour: 'numeric', 
+    minute: 'numeric', 
+    timeZone: 'UTC',
+}
+
+let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+
+
+let currentData = new Date().toLocaleDateString('ru-RU', options);
+
 timeDiv.textContent = new Date().toLocaleTimeString();
+dateDiv.textContent = days[new Date().getDay()] + ' ' + currentData;
+
+
 
 function showTime(){
-    timeDiv.textContent = new Date().toLocaleTimeString()
+    timeDiv.textContent = new Date().toLocaleTimeString();
+    showDate();
+}
+
+function showDate(){
+    dateDiv.textContent = days[new Date().getDay()] + ' ' + currentData;
 }
 
 setInterval(showTime, 1000);
